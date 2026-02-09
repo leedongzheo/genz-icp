87312a8837857c4ca341ac7dae02893d6cdf2db2
leedongzheo <125799135+leedongzheo@users.noreply.github.com>
2026-02-09 18:02:45 +0700

    Update config.py

--- python/genz_icp/config/config.py
+++ python/genz_icp/config/config.py
@@ -4,24 +4,24 @@ from typing import Optional
 class DataConfig(BaseModel):
     max_range: float = 100.0
     min_range: float = 0.5
-    # deskew: bool = False
-    deskew: bool = True
+    deskew: bool = False
+    # deskew: bool = True
 
 
 class MappingConfig(BaseModel):
     # voxel_size: float = 0.25
-    # voxel_size: float = 0.5
-    voxel_size: Optional[float] = None
+    voxel_size: float = 0.5
+    # voxel_size: Optional[float] = None
     # map_cleanup_radius: float = 400.0
     map_cleanup_radius: float = 100.0
-    # max_points_per_voxel: int = 1
-    max_points_per_voxel: int = 20
+    max_points_per_voxel: int = 1
+    # max_points_per_voxel: int = 20
     desired_num_voxelized_points: int = 2000
 
 
 class RegistrationConfig(BaseModel):
-    # max_num_iterations: int = 150
-    max_num_iterations: int = 500
+    max_num_iterations: int = 150
+    # max_num_iterations: int = 500
     convergence_criterion: float = 0.0001
 
 
